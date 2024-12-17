2b63684b8d3d4a93479590270bced94f19d4e150
Mykhailo Forkosh <forkos277@gmail.com>
2024-12-17 21:01:23 +0200

    make build

---
+++ index.aa68c06b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.clientWidth/2-e.clientWidth/2,l=t.clientHeight/2-e.clientHeight/2;e.style.top="".concat(l,"px"),e.style.left="".concat(c,"px");
+//# sourceMappingURL=index.aa68c06b.js.map
