ae04a6a58e635480c4964b9d79bc5951ee05f4e9
Mykhailo Forkosh <forkos277@gmail.com>
2024-12-17 21:01:08 +0200

    solution 2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,5 @@ const wall = document.querySelector('.wall');
 const centerSpiderhorizont = wall.clientWidth / 2 - spider.clientWidth / 2;
 const centerSpidervertical = wall.clientHeight / 2 - spider.clientHeight / 2;
 
-spider.style.top = `${centerSpiderhorizont}px`;
-spider.style.left = `${centerSpidervertical}px`;
+spider.style.top = `${centerSpidervertical}px`;
+spider.style.left = `${centerSpiderhorizont}px`;
