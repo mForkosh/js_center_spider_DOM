f6ec4614e6f601367b1a377246d4843d6acdf26f
Mykhailo Forkosh <forkos277@gmail.com>
2024-12-17 20:55:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const centerSpiderhorizont = wall.clientWidth / 2 - spider.clientWidth / 2;
+const centerSpidervertical = wall.clientHeight / 2 - spider.clientHeight / 2;
+
+spider.style.top = `${centerSpiderhorizont}px`;
+spider.style.left = `${centerSpidervertical}px`;
